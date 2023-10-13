86a2fe75b1f424b42b15438c6dcad67d7ad96705
Anis REMIKI <anremiki@made-f0Cr12s5.clusters.42paris.fr>
2023-10-13 06:20:31 +0200

    .

--- frontend/src/components/modalChat/FetchAPiChat.tsx
+++ frontend/src/components/modalChat/FetchAPiChat.tsx
@@ -106,6 +106,7 @@ const acceptFriendRequest = async (requesterId : any) => {
     } catch (error) {
         handleAxiosError(error);
     }
+  }
 
 
   const getPendingFriends = async () => {
@@ -134,8 +135,7 @@ const acceptFriendRequest = async (requesterId : any) => {
     } catch (error) {
         handleAxiosError(error);
     }
-}
-};
+  }
   
   export {
     sendFriendRequest,
@@ -148,5 +148,5 @@ const acceptFriendRequest = async (requesterId : any) => {
     getDataByPseudoApi,
     getAcceptedFriends,
     getPendingFriends,
-    getBlockedUSers
+    getBlockedUsers,
   };
