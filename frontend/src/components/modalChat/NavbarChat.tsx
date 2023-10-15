a86842d32cc749f6dcdb3f172471c7f00c7a057f
Anis REMIKI <anremiki@made-f0Cr6s3.clusters.42paris.fr>
2023-10-15 07:48:46 +0200

    .

--- frontend/src/components/modalChat/NavbarChat.tsx
+++ frontend/src/components/modalChat/NavbarChat.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import * as API from './FetchAPiChat';
 import './Chatbox.css';
 import { BiSolidCrown } from 'react-icons/bi';
+import { IconType } from 'react-icons';
 import { FaUsers, FaUser, FaUserTie, FaUserAltSlash, FaUserCog, FaUserMinus, FaUserPlus } from 'react-icons/fa';
 
 interface ConversationProps {
@@ -9,6 +10,7 @@ interface ConversationProps {
 	onClick: () => void;
 	id?: string;
 	message: string;
+	icon?: React.ReactNode;
 }
 
 interface ChatProps {
@@ -37,7 +39,7 @@ function OptionConversation({onClick, message}: ConversationProps) {
 	)
 }
 
-function ButtonConversation({onClick, message, id}: ConversationProps) {
+function ButtonConversation({onClick, message, id, icon}: ConversationProps) {
 
 	return (
 		<button
@@ -45,7 +47,8 @@ function ButtonConversation({onClick, message, id}: ConversationProps) {
 		key={id}
 		onClick={onClick}
 		className='hover:bg-white/10 h-10 min-h-[4rem] w-[96%] mx-1 bg-white/20 border border-1 mt-4 flex items-center justify-center text-white'>
-				{message}
+			{icon}
+			{message}
 		</button>
 	)
 }
@@ -59,8 +62,8 @@ export default function NavbarChat({selectedChat, setSelectedChat}: ChatProps) {
 
 		const getMyChannels = async () => {
 			const fetchedMyChannels : any = await API.getMyChannels(); //tmp value need real userId
-			console.log("FETCHED: ", fetchedMyChannels.message);
-			return fetchedMyChannels?.message;
+			console.log("FETCHED: ", fetchedMyChannels);
+			return fetchedMyChannels;
 		}
 
 		getMyChannels().then((result) => {
@@ -83,11 +86,12 @@ export default function NavbarChat({selectedChat, setSelectedChat}: ChatProps) {
 				console.log('find conv');
 				setSelectedChat(-2);
 			}}/>
-			{myChannels && myChannels.map((channel: Channel) => (
+			{myChannels && myChannels.map((channel: any, index: number) => (
 				<ButtonConversation
-				id={channel.id}
-				key={channel.id}
-				message={channel.name}
+				id={`${index}`}
+				key={index}
+				message={channel.channelName}
+				icon={channel.ownerId === myUser.id ? <BiSolidCrown /> : undefined}
 				onClick={() => {
 					console.log(`set conv ${channel.id}`);
 					setSelectedChat(channel.id);
